9b2d97d2bd090b29d43dfb6b801c89e7ffd3e4f7
rbait31 <rbait31@users.noreply.github.com>
2025-12-17 17:53:46 -0800

    Text change

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         Добро пожаловать в Referent
       </h1>
       <p style={{ fontSize: '1.2rem', color: '#666' }}>
-        Минимальное приложение на Next.js
+        Я изучаю Next.js
       </p>
     </main>
   )
